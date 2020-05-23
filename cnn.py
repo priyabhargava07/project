1ef920ed2e8eaa0b5e87964a76f22dbe949f0178
priyabhargava07 <53579199+priyabhargava07@users.noreply.github.com>
2020-05-24 00:24:28 +0530

    cnn code2

--- cnn.py
+++ cnn.py
@@ -2,122 +2,43 @@
 
 
 # coding: utf-8
+In[1]:import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
 
-
-
- In[1]:
-
-
-import tensorflow as tf
-
- 
-        from tensorflow.keras import datasets, layers, models
- 
-        import matplotlib.pyplot as plt
-
-
-
-
- In[2]:
-
-
-(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
-
-
-       # Normalize pixel values to be between 0 and 1
-        
+In[2]:(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+# Normalize pixel values to be between 0 and 1
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
-
-
-
- In[3]:
-
-
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-'dog', 'frog', 'horse', 'ship', 'truck']
-
-       plt.figure(figsize=(10,10))
+In[3]:class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer','dog', 'frog', 'horse', 'ship', 'truck']
+plt.figure(figsize=(10,10))
 for i in range(25):
-   
-       plt.subplot(5,5,i+1)
-    
-       plt.xticks([])
-   
-       plt.yticks([])
-   
-       plt.grid(False)
-    
-       plt.imshow(train_images[i], cmap=plt.cm.binary)
-    
-       # The CIFAR labels happen to be arrays, 
-    
-       # which is why you need the extra index
-  
-       plt.xlabel(class_names[train_labels[i][0]])
+ plt.subplot(5,5,i+1)
+ plt.xticks([])
+ plt.yticks([])
+ plt.grid(False)
+ plt.imshow(train_images[i], cmap=plt.cm.binary)
+# The CIFAR labels happen to be arrays, which is why you need the extra index
+ plt.xlabel(class_names[train_labels[i][0]])
 plt.show()
 
-
-
-
- In[4]:
-
-
-model = models.Sequential()
-
-       model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-
-       model.add(layers.MaxPooling2D((2, 2)))
+In[4]:model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
-       model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+In[5]:model.summary()
 
+In[6]:model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
 
+In[7]:model.summary()
 
+ In[8]:model.compile(optimizer='adam',loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),metrics=['accuracy'])
+ history = model.fit(train_images, train_labels, epochs=2,validation_data=(test_images, test_labels))
 
- In[5]:
-
-
-model.summary()
-
-
-
-
- In[6]:
-
-
-model.add(layers.Flatten())
-
-       model.add(layers.Dense(64, activation='relu'))
-
-       model.add(layers.Dense(10))
-
-
-
-
- In[7]:
-
-
-model.summary()
-
-
-
-
- In[8]:
-
-
-model.compile(optimizer='adam',
-loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),metrics=['accuracy'])
-
-
-       history = model.fit(train_images, train_labels, epochs=2,validation_data=(test_images, test_labels))
-
-
-
-
- In[10]:
-
-
-print(test_acc)
+ In[9]:print(test_acc)
 
